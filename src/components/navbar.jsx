1e94029f58b9899af5c8d1e6b8538b3741b17191
Angus Logan <angus.logan@baronsview.com>
2022-10-21 00:00:50 +0100

    feat: first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <div className='w-1/6 h-screen p-4 pt-10 fixed border-r-[1px] border-neutral-500 text-right'>
+            <p className='text-neutral-50 text-xl'>Angus Logan</p>
+            <p className='text-neutral-50 font-light'>third year <span className='text-fuchsia-400'>Computer Science</span> student @ the University of Strathclyde</p>
+
+            <div className='mt-20'>
+                <ul>
+                    <li><button className='text-neutral-50 text-xl mb-4 hover:text-fuchsia-400'><Link to="/">Home</Link></button></li>
+                    <li><button className=' text-neutral-50 text-xl mb-4 hover:text-fuchsia-400'><Link to="/technologies">Technologies</Link></button></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Technologies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Navbar from '../components/navbar'
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Technologies</h1>
+        </div>
+    )
+}
+
+export default Home
