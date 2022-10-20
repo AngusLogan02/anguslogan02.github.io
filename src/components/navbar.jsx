5ee3f254d09d5ff1f2acffdc687dcef3bded88ee
Angus Logan <angus.logan@baronsview.com>
2022-10-21 00:25:32 +0100

    fix: updated technologies style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className='w-1/6 h-screen p-4 pt-10 fixed border-r-[1px] border-neutral-500 text-right'>
-            <p className='text-neutral-50 text-xl'>Angus Logan</p>
+            <p className='text-neutral-50 text-xl font-bold'>Angus Logan</p>
             <p className='text-neutral-50 font-light'>third year <span className='text-fuchsia-400'>Computer Science</span> student @ the University of Strathclyde</p>
 
             <div className='mt-20'>
--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Navbar from '../components/navbar'
 const Home = () => {
     return (
-        <div>
+        <div className='w-screen h-screen bg-black'>
             <Navbar />
             <h1>Technologies</h1>
         </div>
