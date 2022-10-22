3c4bc4f251f93b5e2995b1a4242dfef478dec318
Angus Logan <angus.logan@baronsview.com>
2022-10-22 17:33:06 +0100

    fix: technologies exported home

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Home = () => {
+const Technologies = () => {
     return (
         <h1>Technologies</h1>
     )
 }
 
-export default Home
+export default Technologies
