fbef8106dfd8388c29cfed33550cffe1f27fcc58
Angus Logan <angus.logan@baronsview.com>
2022-10-22 16:05:50 +0100

    feat: added social links to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { AiOutlineGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai"
+import { BsDiscord } from "react-icons/bs"
+import { MdAlternateEmail } from "react-icons/md"
 
 const Navbar = () => {
     return (
@@ -13,6 +16,13 @@ const Navbar = () => {
                     <li><button className='text-xl mb-4 link-underline'><Link to="/technologies">Technologies</Link></button></li>
                     <li><button className='text-xl mb-4 link-underline'><Link to="/contact">Contact Me</Link></button></li>
                 </ul>
+            </div>
+
+            <div className='absolute bottom-20 -ml-4 w-full flex text-center justify-evenly'>
+                <a className='hover:text-fuchsia-400 duration-300' href="https://github.com/anguslogan02"><AiOutlineGithub size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="https://discordapp.com/users/211862681731203073"><BsDiscord size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="https://www.linkedin.com/in/angus-logan-b83020226/"><AiFillLinkedin size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="mailto:me@anguslogan.dev"><MdAlternateEmail size={25} /></a>
             </div>
         </div>
     )
