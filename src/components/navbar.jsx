af88820dd2f7921effde006dfe8ab438719008a2
Angus Logan <angus.logan@baronsview.com>
2022-10-22 18:40:26 +0100

    feat: about me

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,22 +7,22 @@ import { MdAlternateEmail } from "react-icons/md"
 const Navbar = () => {
     return (
         <div className='w-1/6 h-screen p-4 pt-20 fixed border-r-[1px] border-neutral-500 text-right'>
-            <a href='https://github.com/anguslogan02' target="blank" className='text-xl font-bold link-underline'>Angus Logan</a>
-            <p className='font-light'>third year <span className='text-fuchsia-400'>Computer Science</span> student @ the University of Strathclyde</p>
+            <a href='https://github.com/anguslogan02' target="_blank" className='text-xl font-bold link-underline'>Angus Logan</a>
+            <p className='font-light mt-2'>third year <span className='text-fuchsia-400'>Computer Science</span> student @ the University of Strathclyde</p>
 
-            <div className='mt-20'>
+            <div className='mt-4'>
                 <ul>
-                    <li><button className='text-xl mb-4 link-underline'><Link to="/">Home</Link></button></li>
+                    <li><button className='text-xl mb-4 link-underline'><Link to="/">About Me</Link></button></li>
                     <li><button className='text-xl mb-4 link-underline'><Link to="/technologies">Technologies</Link></button></li>
                     <li><button className='text-xl mb-4 link-underline'><Link to="/contact">Contact Me</Link></button></li>
                 </ul>
             </div>
 
             <div className='absolute bottom-20 -ml-4 w-full flex text-center justify-evenly'>
-                <a className='hover:text-fuchsia-400 duration-300' href="https://github.com/anguslogan02" target="blank"><AiOutlineGithub size={25} /></a>
-                <a className='hover:text-fuchsia-400 duration-300' href="https://discordapp.com/users/211862681731203073" target="blank"><BsDiscord size={25} /></a>
-                <a className='hover:text-fuchsia-400 duration-300' href="https://www.linkedin.com/in/angus-logan-b83020226/" target="blank"><AiFillLinkedin size={25} /></a>
-                <a className='hover:text-fuchsia-400 duration-300' href="mailto:me@anguslogan.dev" target="blank"><MdAlternateEmail size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="https://github.com/anguslogan02" target="_blank"><AiOutlineGithub size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="https://discordapp.com/users/211862681731203073" target="_blank"><BsDiscord size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="https://www.linkedin.com/in/angus-logan-b83020226/" target="_blank"><AiFillLinkedin size={25} /></a>
+                <a className='hover:text-fuchsia-400 duration-300' href="mailto:me@anguslogan.dev" target="_blank"><MdAlternateEmail size={25} /></a>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className='max-w-xl'>
+            <h1 className='text-3xl mb-4'>I am a</h1>
+            <p>
+                20 year old Computer Science student with an interest in web applications, linux and Golang.
+                I've written web and command line applications in Golang, Python, Java and C that make use of
+                various <span className='link-underline font-bold'><Link to="/technologies">technologies</Link></span>.
+            </p>
+            <p className='mt-4'>
+                Everything I have made so far is open source, and can be found on 
+                my <a href = "https://github.com/anguslogan02" className='link-underline font-bold' target="_blank">GitHub</a>.
+                I like the idea of FOSS, and strive to keep all my projects open and free.
+            </p>
+            <p className='mt-4'>
+                When I'm not doing university work or working on a project of mine, I enjoy playing the guitar,
+                lifting, playing games, and tinkering with hardware and software. Most notably I've made a button box
+                from an <span className='font-bold link-underline'><Link to="/technologies">Arduino</Link></span> to use with a flight simulator game,
+                and a virtualisation server using <span className='font-bold link-underline'><Link to="/technologies">Proxmox</Link></span>, which I've
+                used to run game servers, a NAS and thin clients.
+            </p>
+        </div>
     )
 }
 
